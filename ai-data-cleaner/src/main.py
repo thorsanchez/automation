c68b32260e60902c9219938a8619a2c4f6279b73
thorsanchez <ths281@hi.is>
2025-12-17 15:51:34 +0000

    main

---
+++ ai-data-cleaner/src/main.py
@@ -0,0 +1,27 @@
+import pandas as pd
+from clean_text import clean_text
+from ai_enrich import analyze_text
+
+input_path ="data/input.csv"
+output_path = "data/output.csv"
+
+def main():
+    df = pd.read_csv(input_path)
+    #clean txt
+    df["clean_text"] = df["clean_text"].apply(clean_text) #tekka
+    sentiments = []
+    summaries = []
+
+    for text in df["clean_text"]:
+        #senda i llm
+        result = analyze_text(text)
+        #get() og ekki result[] (crash)
+        sentiments.append(result.get("sentiment"))
+        summaries.append(result.get("summary"))
+        #bæta við cols i data frame
+        df["sentiment"] = sentiments
+        df["summary"] = summaries
+        df.to_csv(output_path, index=False)
+
+if __name__ == "__main__":
+    main()
