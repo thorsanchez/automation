01606b1e0ff82140180037da624abed79afd975f
thorsanchez <ths281@hi.is>
2026-01-05 12:44:17 +0000

    Er automated núna, getur tekið inn hvaða dataset sem er

--- automation-statistics/insurance_data.py
+++ automation-statistics/insurance_data.py
@@ -1,19 +1,35 @@
 import pandas as pd
 
 insurance_csv = "insurance.csv"
-df = pd.read_csv(insurance_csv)
+nba_salary = "nba_2022-23_all_stats_with_salary.csv"
+df = pd.read_csv(nba_salary)
 
 #print(df.head())
 #print(df.describe())
 #age,sex,bmi,children,smoker,region,charges
 
 def univariate(df):
-    output_df = pd.DataFrame(columns=['type', 'count', 'missing', 'unique'])
+    output_df = pd.DataFrame(columns=['type', 'count', 'missing', 'unique', 'mode', 'min', 'q1', 'median', 'q3', 'max', 'mean', 'std', 'skew', 'kurt'])
     for col in df:
         dtype = df[col].dtype
         count = df[col].count()
         missing = df[col].isna().sum()
         unique = df[col].nunique()
-        output_df.loc[col] = [dtype, count, missing, unique]
+        mode = df[col].mode()[0]
+
+        if pd.api.types.is_numeric_dtype(df[col]):
+            min = df[col].min()
+            q1 = df[col].quantile(.25)
+            median = df[col].median()
+            q3 = df[col].quantile(.75)
+            max = df[col].max()
+            mean = df[col].mean()
+            std = df[col].std()
+            skew = df[col].skew()
+            kurt = df[col].kurt()
+            
+            output_df.loc[col] = [dtype, count, missing, unique, mode, min, q1, median, q3, max, mean, std, skew, kurt]
+        else:
+            output_df.loc[col] = [dtype, count, missing, unique, mode, '-', '-', '-', '-', '-', '-', '-', '-', '-']
     return output_df
 print(univariate(df))
