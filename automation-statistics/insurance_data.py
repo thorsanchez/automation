81c08d208c2582bf458d918e3faa0ca9b10de088
thorsanchez <ths281@hi.is>
2026-01-05 11:59:06 +0000

    function til að skoða data types count missing og unique gildi

--- automation-statistics/insurance_data.py
+++ automation-statistics/insurance_data.py
@@ -3,4 +3,16 @@ import pandas as pd
 insurance_csv = "insurance.csv"
 df = pd.read_csv(insurance_csv)
 
-print(df)
+#print(df.head())
+#print(df.describe())
+#age,sex,bmi,children,smoker,region,charges
+
+def univariate(df):
+    output_df = pd.DataFrame(columns=['type', 'count', 'missing', 'unique'])
+    for col in df:
+        dtype = df[col].dtype
+        count = df[col].count()
+        missing = df[col].isna().sum()
+        unique = df[col].nunique()
+        print(dtype, count, missing, unique)
+print(univariate(df))
